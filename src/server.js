5a14e6cb6dbf54113fc7326205c250d8af7b675c
FornesBorja <fornes.borja@gmail.com>
2024-07-10 11:57:38 +0200

    update: format readme

--- src/server.js
+++ src/server.js
@@ -5,6 +5,15 @@ const app=express()
 
 const PORT= process.env.PORT||4000
 
+app.get('/healthy', (req, res)=>{
+    res.json(
+        {
+            success:true,
+            message:"Server is healthy"
+        }
+    )
+})
+
 app.listen(PORT, ()=>{
     console.log(`Server is runing in ${PORT}`)
 })
