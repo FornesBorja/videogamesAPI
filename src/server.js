b0c88061d99230ff5cd760765a4f5f1a9f0d0bba
FornesBorja <fornes.borja@gmail.com>
2024-07-10 12:14:19 +0200

    feat: connect DB to server.js

--- src/server.js
+++ src/server.js
@@ -1,19 +1,21 @@
-import express from 'express';
-import 'dotenv/config'
+import express from "express";
+import "dotenv/config";
+import { dbConnection } from "./database/db";
 
-const app=express()
+const app = express();
 
-const PORT= process.env.PORT||4000
+const PORT = process.env.PORT || 4000;
 
-app.get('/healthy', (req, res)=>{
-    res.json(
-        {
-            success:true,
-            message:"Server is healthy"
-        }
-    )
-})
+app.get("/healthy", (req, res) => {
+  res.json({
+    success: true,
+    message: "Server is healthy",
+  });
+});
 
-app.listen(PORT, ()=>{
-    console.log(`Server is runing in ${PORT}`)
-})
+dbConnection().then(() => {
+  console.log("Database connected")
+  app.listen(PORT, () => {
+    console.log(`Server is runing in ${PORT}`);
+  });
+});
